d4819d50203e23a2bdc957e186364e4c82656950
upkl <2737384+upkl@users.noreply.github.com>
2023-08-13 15:45:13 +0200

    Followed (most) rubocop suggestions

--- stock_picker.rb
+++ stock_picker.rb
@@ -1,11 +1,26 @@
-def stock_picker prices
-  buy_dates = (0...prices.length).select{ |d| d == 0 or (d < prices.length - 1 and prices[d - 1] > prices[d] and prices[d] <= prices[d + 1]) }
-  sell_dates = (0...prices.length).select{ |d| d == prices.length - 1 or (prices.length > 0 and prices[d - 1] <= prices[d] and prices[d] > prices[d + 1]) }
+# frozen_string_literal: true
+
+def get_buy_dates(prices)
+  (0...prices.length).select do |d|
+    d.zero? or (d < prices.length - 1 and prices[d - 1] > prices[d] and prices[d] <= prices[d + 1])
+  end
+end
+
+def get_sell_dates(prices)
+  (0...prices.length).select do |d|
+    d == prices.length - 1 or (!prices.length.empty? and prices[d - 1] <= prices[d] and prices[d] > prices[d + 1])
+  end
+end
+
+def stock_picker(prices)
+  buy_dates = get_buy_dates(prices)
+  sell_dates = get_sell_dates(prices)
+
   best = -1
   result = nil
   buy_dates.each do |b|
     sell_dates.each do |s|
-      if s > b and prices[s] - prices[b] > best
+      if s > b && prices[s] - prices[b] > best
         result = b, s
         best = prices[s] - prices[b]
       end
--- substrings.rb
+++ substrings.rb
@@ -1,9 +1,10 @@
-def substrings word, dictionary
+# frozen_string_literal: true
+
+def substrings(word, dictionary)
   w = word.downcase
-  dictionary.reduce({}) do |result, d|
+  dictionary.each_with_object({}) do |d, result|
     (0...(w.length - d.length)).each do |i|
       result[d] = result.fetch(d, 0) + 1 if w[i, d.length] == d
     end
-    result
   end
 end
